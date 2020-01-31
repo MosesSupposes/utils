2a2250bcc553664816fd39630ccf9c60d7e15909
Moses Samuel <mosamuel98@gmail.com>
2020-01-31 12:38:31 -0800

    add [empty] test for allowing for an array param

--- tests/without.test.js
+++ tests/without.test.js
@@ -10,6 +10,10 @@ it('filters out the specified property from the object', () => {
 
 })
 
+it('can accept an array of keys to filter out', () => {
+    
+})
+
 it('doesn\'t mutate the original object', () => {
 
 })
