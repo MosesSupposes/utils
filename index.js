7b05a56e891edb3ee86c11e32149f2be9f049f59
Moses Samuel <mosamuel98@gmail.com>
2020-03-05 20:50:55 -0800

    account for an edge case in the curry util

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 */
 
 export const curry = (fn, seen = []) => (...args) =>
-  fn.length === args.length + seen.length
+  fn.length >= args.length + seen.length
     ? fn(...seen, ...args)
     : curry(fn, [...seen, ...args]);
 
