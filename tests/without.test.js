d53a4a5e4fdd3f4215a629c52907f33dca4de8a8
Moses A. Samuel <mosamuel98@gmail.com>
2020-03-20 18:26:56 -0700

    unit test the without utility

--- tests/without.test.js
+++ tests/without.test.js
@@ -1,19 +1,25 @@
-it('returns an object', () => {
+import { without } from "../index";
 
-})
+it("returns an object", () => {
+	expect(typeof without("foo", { foo: "bar" })).toBe("object");
+});
 
-it('is curryable', () => {
+it("is curryable", () => {
+	expect(typeof without("foo")).toBe("function");
+});
 
-})
+it("filters out the specified property from the object", () => {
+	expect(Object.keys(without("a", { a: 1, b: 2, c: 3 }))).not.toContain("a");
+});
 
-it('filters out the specified property from the object', () => {
+it("can accept an array of keys to filter out", () => {
+	const actual = without(["a", "b"], { a: 1, b: 2, c: 3 });
+	const expected = { c: 3 };
+	expect(actual).toEqual(expected);
+});
 
-})
-
-it('can accept an array of keys to filter out', () => {
-    
-})
-
-it('doesn\'t mutate the original object', () => {
-
-})
+it("doesn't mutate the original object", () => {
+	const original = { a: 1, b: 2, c: 3 };
+	without("a", original);
+	expect(original).toEqual({ a: 1, b: 2, c: 3 });
+});
